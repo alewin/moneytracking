5d50b6549dc555004b43eb49c1d8c9e2c5166168
alewin <ko.alessio@gmail.com>
2017-08-16 10:56:20 +0200

    added datapicker

--- app/src/main/java/com/unibo/koci/moneytracking/Activities/NewItemActivity.java
+++ app/src/main/java/com/unibo/koci/moneytracking/Activities/NewItemActivity.java
@@ -1,5 +1,7 @@
 package com.unibo.koci.moneytracking.Activities;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
@@ -7,6 +9,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -57,6 +61,29 @@ public class NewItemActivity extends AppCompatActivity implements
         mPlaceArrayAdapter = new PlaceAdapter(this, android.R.layout.simple_list_item_1,
                 BOUNDS_MOUNTAIN_VIEW, null);
         mAutocompleteTextView.setAdapter(mPlaceArrayAdapter);
+
+
+        final EditText dateInputText = (EditText)findViewById(R.id.check_date);
+        dateInputText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                //To show current date in the datepicker
+                Calendar mcurrentDate=Calendar.getInstance();
+                int mYear=mcurrentDate.get(Calendar.YEAR);
+                int mMonth=mcurrentDate.get(Calendar.MONTH);
+                int mDay=mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog mDatePicker=new DatePickerDialog(NewItemActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+                      dateInputText.setText(selectedday + "/" + selectedmonth + "/" + selectedyear);
+                    }
+                },mYear, mMonth, mDay);
+                mDatePicker.setTitle("Select date");
+                mDatePicker.show();  }
+        });
+
     }
 
     private AdapterView.OnItemClickListener mAutocompleteClickListener
